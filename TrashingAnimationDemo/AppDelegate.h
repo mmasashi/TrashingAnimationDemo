12ad30dee55a52e00de543610e51de0dd9952c37
mmasashi <mmasashi@gmail.com>
2011-11-25 23:50:01 +0900

    Initial Commit

---
+++ TrashingAnimationDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TrashingAnimationDemo
+//
+//  Created by Miyazaki Masashi on 11/11/25.
+//  Copyright (c) 2011 mmasashi.jp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TrashingAnimationDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TrashingAnimationDemo
+//
+//  Created by Miyazaki Masashi on 11/11/25.
+//  Copyright (c) 2011 mmasashi.jp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
